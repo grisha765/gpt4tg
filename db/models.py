1fb28cdb33826e32156f062245db3124607eb911
Grisha <grisha765@tuta.io>
2025-01-13 23:56:14 +0300

    [+] add username func

--- db/models.py
+++ db/models.py
@@ -1,17 +1,6 @@
 from tortoise import fields
 from tortoise.models import Model
 
-"""
-class UserOption(Model):
-    id = fields.IntField(pk=True)
-    user_id = fields.BigIntField()
-    username = fields.CharField(max_length=8)
-
-    class Meta:
-        table = "username"
-        unique_together = ("user_id", "username")
-"""
-
 class ActivateGroup(Model):
     id = fields.IntField(pk=True)
     chat_id = fields.BigIntField(unique=True)
@@ -21,6 +10,16 @@ class ActivateGroup(Model):
     class Meta: #type: ignore
         table = "activate_group"
         unique_together = ("chat_id", "password")
+
+class UserName(Model):
+    id = fields.IntField(pk=True)
+    user_id = fields.BigIntField(unique=True)
+    username = fields.CharField(max_length=32)
+    custom = fields.BooleanField()
+
+    class Meta: #type: ignore
+        table = "username"
+        unique_together = ("user_id", "username")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
---
+++ db/username.py
@@ -0,0 +1,40 @@
+import re
+from db.models import UserName
+from tortoise.exceptions import DoesNotExist
+
+async def set_username(user_id: int, username: str) -> bool:
+    if not (5 <= len(username) <= 32):
+        return False
+    if not re.fullmatch(r"[a-zа-яё0-9]+", username, re.IGNORECASE):
+        return False
+
+    existing_user = await UserName.filter(username=username).first()
+
+    if existing_user:
+        if existing_user.user_id != user_id:
+            if existing_user.custom:
+                await existing_user.delete()
+            else:
+                return False
+
+    user_record = await UserName.filter(user_id=user_id).first()
+
+    if user_record:
+        user_record.username = username
+        user_record.custom = True
+        await user_record.save()
+    else:
+        await UserName.create(user_id=user_id, username=username, custom=False)
+
+    return True
+
+
+async def check_username(user_id: int):
+    try:
+        user_record = await UserName.get(user_id=user_id)
+        return user_record.username
+    except DoesNotExist:
+        return False
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")
