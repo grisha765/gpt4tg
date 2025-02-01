f60fb1304c854402d258140aa6af240136084246
Grisha <grisha765@tuta.io>
2025-02-01 04:23:23 +0300

    [u] add chat table

--- db/models.py
+++ db/models.py
@@ -21,5 +21,16 @@ class UserName(Model):
         table = "username"
         unique_together = ("user_id", "username")
 
+class Chat(Model):
+    id = fields.IntField(pk=True)
+    chat_id = fields.BigIntField(index=True)
+    message_id = fields.BigIntField(unique=True)
+    username = fields.CharField(max_length=32)
+    message_text = fields.TextField()
+    reply_to_message_id = fields.BigIntField(null=True)
+
+    class Meta: #type: ignore
+        table = "chat"
+
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
