07cd9ab5d454285257abbf189ac7e871bf95965e
Grisha <grisha765@tuta.io>
2025-02-01 04:27:05 +0300

    [u] implementation analysis func

--- func/gpt_commands.py
+++ func/gpt_commands.py
@@ -1,8 +1,10 @@
 from db.username import set_username, check_username, reset_username
+from func.msg import analysis
+
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-async def command_handler(message, username, command, args):
+async def command_handler(app, message, username, command, args):
     if command == "!setname":
         user_id = message.from_user.id
         if args and len(args) > 0:
@@ -15,9 +17,11 @@ async def command_handler(message, username, command, args):
             if await set_username(user_id, new_username):
                 await message.reply(f"{username} has set a new username: {new_username}")
             else:
-                await message.reply(f"Error set a new username.\nUse only letters and numbers, minimum 5 characters.")
+                await message.reply(f"Error set a new username.\nUse only letters and numbers, minimum 5 characters, maximum 32 characters.")
         else:
             await message.reply(f"Your Username: {await check_username(user_id)}") 
+    elif command == "!analysis":
+        await analysis(app, message)
     else:
         await message.reply("Unknown command.")
 
